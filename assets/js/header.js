f351c8f193e8b4aac5d76c1aef4017f01d499106
zahid202232 <101721745+zahid202232@users.noreply.github.com>
2024-04-19 23:47:58 +0500

    new changes

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,6 +2,7 @@
 $("#hamburger").click(function(event) {
     event.preventDefault();
     $("#nav").addClass("showNav");
+    $("#mobileMenu").show();
     var winHeight = $(window).outerHeight();
     // Set the window height of the mobile menu when engaged!
     $('#menuWrapper').css('height',winHeight + 'px');
@@ -10,6 +11,8 @@ $("#hamburger").click(function(event) {
 $("#close").click(function(event) {
     event.preventDefault();
     $("#nav").removeClass("showNav");
+    $("#mobileMenu").hide();
+
     // Set the window height of the mobile menu when not engaged!
     $('#menuWrapper').css('height','auto');
 });
