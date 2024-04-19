274aab648fe2ecd977ae8128ed0b8f88da6dee4b
zahidhussain36328 <119063238+zahidhussain36328@users.noreply.github.com>
2024-04-19 14:33:31 +0500

    upload asset files

---
+++ assets/js/header.js
@@ -0,0 +1,26 @@
+/* Init the mobile menu */
+$("#hamburger").click(function(event) {
+    event.preventDefault();
+    $("#nav").addClass("showNav");
+    var winHeight = $(window).outerHeight();
+    // Set the window height of the mobile menu when engaged!
+    $('#menuWrapper').css('height',winHeight + 'px');
+});
+
+$("#close").click(function(event) {
+    event.preventDefault();
+    $("#nav").removeClass("showNav");
+    // Set the window height of the mobile menu when not engaged!
+    $('#menuWrapper').css('height','auto');
+});
+
+/* On hover apply a class to the dropdown '.hov' */
+$('#menuWrapper ul li').hover( function () {
+var el = $(this).children('ul');
+// check if it has a class of .hov 
+if (el.hasClass('hov')) {
+    $(el).removeClass('hov');
+} else {
+    $(el).addClass('hov');
+}
+});
